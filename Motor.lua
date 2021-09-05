1559b54575c50bcae2efd8f4b33b5013291cc7b1
MAP <74914349+MAP233224@users.noreply.github.com>
2021-09-05 16:23:54 +0200

    Update Motor.lua

--- Motor.lua
+++ Motor.lua
@@ -6,13 +6,16 @@
 
 --------------------------------
 
-user_seed = 0x2AE944A9 --enter the seed you got from Motor.exe, press '5' to set it
-user_pointer = 0 --automatically search for a specific ASLR
+user_seed = 0xCA9D7ED6 --enter the seed you got from Motor.exe, press '5' to set it
+user_pointer = 0 --0x0227116c --automatically search for a specific ASLR
 user_tid = 2880 --enter a custom TID, press '3' to set it
 user_sid = 33456 --enter a custom SID, press '3' to set it
 
 --------------------------------
 
+mode=1 --switch between Decrypted Battle Data and Encrypted Wild Pokémon Data
+MODE_MAX = 3
+MODES = {"Encrypted Wild Data", "Decrypted Battle Data", "Nothing"}
 tabl = {}
 -- {dp, plat} --
 RNG_OFF = {0xBDD88, 0xBDDD4}
@@ -37,34 +40,31 @@ MULTSPB = {
 
 BLOCKS = {"ABCD", "ABDC", "ACBD", "ACDB", "ADBC", "ADCB", "BACD", "BADC", "BCAD", "BCDA", "BDAC", "BDCA", "CABD", "CADB", "CBAD", "CBDA", "CDAB", "CDBA", "DABC", "DACB", "DBAC", "DBCA", "DCAB", "DCBA"}
 
+BASES = {{0x02107100, 0x02101EE0}, {0x02106FC0, 0x02101D40}, {0x02107140, 0x02101F20}, {0x021070A0, 0x02101EA0}, {0x02108818, 0x02101140}, {0x021045C0, 0x02102C40}, {0x02107160, 0x02101F40}}
+-- dp then pt, in this order: ge, en, fr, it, jp, ko, sp (need to change it later to conventional order)
+
+PKMN_BD_OFF = {0x48580, 0x54598} -- +0xC0 to get the opponent
+CLIENT_NAME = {"Trainer", "Opponent"}
+
+LANG_ID = {0x44, 0x45, 0x46, 0x49, 0x4A, 0x4B, 0x53}
+
+function IndexOf(array, value)
+  local index={}
+  for k,v in pairs(array) do index[v]=k end
+  return index[value]
+end
+
 function GetGameInfo()
   -- to do: set the global OFFs and LOCs to their corresponding values
   local vers = memory.readdword(0x023FFE0C)
   local id = bit.band(vers, 0xFF)
   local lang = bit.band(bit.rshift(vers, 24), 0xFF)
+  lang=IndexOf(LANG_ID, lang)
   local b=0
-  if id == 0x41 then -- Diamond & Pearl
-    if lang == 0x44 then b = 0x02107100 -- german
-    elseif lang == 0x45 then b = 0x02106FC0 -- english
-    elseif lang == 0x46 then b = 0x02107140 -- french
-    elseif lang == 0x49 then b = 0x021070A0 -- italian
-    elseif lang == 0x4A then b = 0x02108818 -- japanese
-    elseif lang == 0x4B then b = 0x021045C0 -- korean
-    elseif lang == 0x53 then b = 0x02107160 -- spanish
-    end
-    vers=1
-  elseif id == 0x43 then -- Platinum
-    if lang == 0x44 then b = 0x02101EE0 -- german
-    elseif lang == 0x45 then b = 0x02101D40 -- english
-    elseif lang == 0x46 then b = 0x02101F20 -- french
-    elseif lang == 0x49 then b = 0x02101EA0 -- italian
-    elseif lang == 0x4A then b = 0x02101140 -- japanese
-    elseif lang == 0x4B then b = 0x02102C40 -- korean
-    elseif lang == 0x53 then b = 0x02101F40 -- spanish
-    end
-    vers=2
-  else print("Invalid game!")
-  end
+  if id==0x41 then vers=1 -- Diamond & Pearl
+  elseif id==0x43 then vers=2 -- Platinum
+  else print("Invalid game!") return end
+  b = BASES[lang][vers]
   return b, vers
 end
 
@@ -107,14 +107,10 @@ function GetSeedDistance(a)
 		if GetBits(a, j, 1) ~= GetBits(test, j, 1) then
 			test = Mult32(test, MULTSPA[j+1]) + MULTSPB[j+1]
 			distseed = distseed + bit.lshift(1,j)
-			if j == 31 then
-				distseed = distseed + 0x100000000
-			end
+			if j==31 then distseed = distseed + 0x100000000 end
 		end
   end
-	if distseed > 0x7FFFFFFF then
-		distseed = distseed - 0xFFFFFFFF
-	end
+	if distseed>0x7FFFFFFF then distseed = distseed - 0xFFFFFFFF end
 	return distseed
 end
 
@@ -161,43 +157,33 @@ function SetTid()
 end
 
 function GetWildData()
-  gui.box(8, 8, 248, 184, "#00000088", "black")
 	for i=0,234, 2 do
-		x = (i%16)*CHAR_W
-		y = math.floor(i/16)*CHAR_H
-    v = memory.readword(wild_loc+i)
-    if i < 4 then
-      color = "green"
-    elseif i < 8 then
-      color = "yellow"
-    elseif i%32 == 8 and i < 106 then
-      color = "cyan"
-    elseif i == 140 then
-      color = "orange"
-    else
-      color = "white"
-    end
+		local x = (i%16)*CHAR_W
+		local y = math.floor(i/16)*CHAR_H
+    local v = memory.readword(wild_loc+i)
+    if i<4 then color="green"
+    elseif i<8 then color="yellow"
+    elseif i%32==8 and i<106 then color="cyan"
+    elseif i==140 then color="orange"
+    else color="white" end
 		gui.text(17+x, 17+y, string.format("%.4X", v), color)
 	end
-  pid = memory.readdword(wild_loc)
-  bef = memory.readword(wild_loc+4)
-  b = GetBlockOrder(pid)
-  if bit.band(bef, 4) == 4 then
-    gui.text(17+CHAR_W*14, 17+CHAR_H*14, "BEGG", "red")
-  else
-    gui.text(17+CHAR_W*14, 17+CHAR_H*14, "PKMN", "green")
-  end
+  local pid = memory.readdword(wild_loc)
+  local bef = memory.readword(wild_loc+4)
+  local b = GetBlockOrder(pid)
+  if bit.band(bef, 4)==4 then gui.text(17+CHAR_W*14, 17+CHAR_H*14, "BEGG", "red")
+  else gui.text(17+CHAR_W*14, 17+CHAR_H*14, "PKMN", "green") end
   gui.text(17+CHAR_W*12, 17+CHAR_H*14, b, "red")
 end
 
-function DecryptDump()
-	local pid = memory.readdword(og_wild_loc)
+function DecryptDump(pkmn_loc)
+	local pid = memory.readdword(pkmn_loc)
 	local blockValue = (bit.band(pid, 0x3E000)/8192)%24
-	local badeggflag = memory.readword(og_wild_loc + 4)
+	local badeggflag = memory.readword(pkmn_loc + 4)
 	local bef = bit.band(badeggflag, 4)
 	local chs_1 = bit.band(badeggflag, 1)
 	local chs_2 = bit.band(badeggflag, 2)
-	local checksum = memory.readword(og_wild_loc + 6) -- +6 to get the checksum
+	local checksum = memory.readword(pkmn_loc + 6) -- +6 to get the checksum
 	local prng = Mult32(checksum, 0x41C64E6D) + 0x6073
   print("")
   for i=0, 0x3F do
@@ -205,7 +191,7 @@ function DecryptDump()
     local omod = offset%32
     local odiv = math.floor(i/16)-4
     local block_id = string.sub(BLOCKS[blockValue+1], odiv, odiv)
-    local v_encrypted = memory.readword(og_wild_loc + 8 + offset)
+    local v_encrypted = memory.readword(pkmn_loc + 8 + offset)
     local v_decrypted = bit.bxor(v_encrypted, bit.rshift(prng, 16))
     print(block_id.."_"..string.format("%.2X", omod).." = "..string.format("%.4X", v_decrypted))
     if (offset+2)%32==0 then
@@ -216,42 +202,66 @@ function DecryptDump()
   print("PID = "..string.format("%.8X", pid))
   print("Block order = "..BLOCKS[blockValue+1])
   print("Checksum = "..string.format("%.4X", checksum))
-  if bef == 4 then
-    print("Bad Egg")
-  else
-    print("Regular Pkmn")
+  if bef==4 then print("Bad Egg")
+  else print("Regular Pkmn")
   end
-  if chs_1 == 1 and chs_2 == 2 then
-    print("Skipped checksum")
-  else
-    print("Verified checksum")
+  if chs_1==1 and chs_2==2 then print("Skipped checksum")
+  else print("Verified checksum")
   end
   print("Flag = "..string.format("%.4X", badeggflag))
 end
 
+function ChangeMode()
+  mode = mode+1
+  if mode > MODE_MAX then mode=1 end
+  print("Mode "..mode..": "..MODES[mode]) --debug
+end
+
+function GetPkmnData()
+  if mode==3 then return 0
+  else
+    gui.box(0, 0, 256, 192, "#00000099", "black")
+    if mode==1 then GetWildData()
+    elseif mode==2 then GetBattleData()
+    else return 0 end
+  end
+end
+
+function GetBattleData()
+  for client=1,2,1 do
+  	local range = memory.readbyterange(pointer+PKMN_BD_OFF[version]+(client-1)*0xC0, 0x80)
+  	for i,v in ipairs(range) do
+  		local x = ((i-1)%8)*CHAR_W
+  		local y = math.floor((i-1)/8)*CHAR_H
+  		if i==1 or i==2 then color="red" --species
+  		elseif i==39 or i==40 or i==77 or i==78 or i==81 or i==82 or i==127 or i==128 then color="yellow"
+  		elseif i>2 and i<25 then color="yellow"
+      elseif i>44 and i<53 then color="yellow"
+  		elseif i>84 and i<99 then	color="yellow"
+  		elseif i>108 and i<113 then	color="yellow"
+      elseif i==54 then color="#44ffbb" -- friendship
+  		elseif i==37 or i==38 then color="orange" -- type
+  		elseif i==121 then color="cyan" -- item
+  		elseif i==122 then color="cyan" -- item
+  		elseif i>104 and i<109 then color="green" -- PID
+  		elseif i>116 and i<121 then color="magenta" -- TID/SID at overwritten PID + 0x6C
+  		else color="white" end
+  		gui.text(CHAR_W+x+(client-1)*120, CHAR_H*2+y, string.format("%.2X", v), color)
+  	end
+  	gui.text(CHAR_W+(client-1)*120, CHAR_H, CLIENT_NAME[client])
+  end
+end
+
 function Controls()
   tabl = input.get()
-  if tabl["1"] and not prev["1"] then
-    RngBackwards(1)
-  end
-  if tabl["2"] and not prev["2"] then
-    RngForwards(1)
-	end
-  if tabl["4"] and not prev["4"] then
-    DecryptDump()
-  end
-  if tabl["8"] and not prev["8"] then
-    RngBackwards(16)
-  end
-  if tabl["9"] and not prev["9"] then
-    RngForwards(16)
-  end
-	if tabl["3"] and not prev["3"] then
-    SetTid()
-	end
-  if tabl["5"] and not prev["5"] then
-    SetRng()
-	end
+  if tabl["0"] and not prev["0"] then ChangeMode() end
+  if tabl["1"] and not prev["1"] then RngBackwards(1) end
+  if tabl["2"] and not prev["2"] then RngForwards(1) end
+  if tabl["3"] and not prev["3"] then SetTid() end
+  if tabl["4"] and not prev["4"] then DecryptDump(wild_loc) end
+  if tabl["5"] and not prev["5"] then SetRng() end
+  if tabl["8"] and not prev["8"] then RngBackwards(16) end
+  if tabl["9"] and not prev["9"] then RngForwards(16) end
   prev=tabl
 end
 
@@ -267,7 +277,8 @@ function Main()
   frame = GetRng()
   frame_dist = GetSeedDistance(frame)
   GetTid()
-  GetWildData()
+  GetPkmnData()
+  gui.text(2+7*CHAR_W, -190, "("..(pointer-base)/4-0x598A8..")") --only eng dp accurate
   gui.text(2, -190, "Base = "..string.format("%.8X",pointer))
   gui.text(2, -180, "Seed = "..string.format("%.8X",user_seed))
   gui.text(2, -170, "Dist = "..frame_dist)
