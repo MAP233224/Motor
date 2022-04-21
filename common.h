7b203eadf8932128be89de28fbc98ce6b3d16cef
MAP <74914349+MAP233224@users.noreply.github.com>
2022-04-21 14:01:53 +0200

    Update common.h

--- common.h
+++ common.h
@@ -25,7 +25,7 @@ typedef int32_t s32;
 typedef uint64_t u64;
 typedef int APPSTATUS;
 
-#define MOTOR_VERSION               ("2.0.0")
+#define MOTOR_VERSION               ("Motor v2.1.0")
 #define SEARCH_THREADS_MAX          (8)
 #define PROFILE_SLOTS_MAX           (8)
 #define STRING_LENGTH_MAX           (16)
@@ -35,13 +35,25 @@ typedef int APPSTATUS;
 #define U32_VALUE_MAX               (0xFFFFFFFF)
 #define U32_DIGITS_HEX_MAX          (8)
 #define U32_DIGITS_DEC_MAX          (10)
+#define U64_VALUE_MAX               (0xFFFFFFFFFFFFFFFF)
+#define U64_DIGITS_HEX_MAX          (16)
 #define ASLR_VALUE_MAX              (11) //Global and Japanese
 #define ASLR_VALUE_MAX_KO           (4) //Korean
 #define ASLR_VALUE_MAX_EN           (3) //English, French, Italian, German and Spanish
 #define ASLR_DIGITS_DEC_MAX         (2)
+#define MAC_VALUE_MAX               (0xFFFFFFFFFFFF)
+#define MAC_DIGITS_HEX_MAX          (12)
+#define YEAR_DIGITS_DEC_MAX         (4)
+#define YEAR_VALUE_MIN              (2000)
+#define YEAR_VALUE_MAX              (2099) //2059 for N3DS
 #define FILTER_DIGITS_MAX           (3)
 #define PATH_ABS_LENGTH_MAX         (256)
 #define PATH_REL_LENGTH_MAX         (4 * STRING_LENGTH_MAX)
+#define LANGUAGES_MAX               (9)  // Number of languages: _0, Japanese, English, French, Italian, German, _6, Spanish and Korean
+#define LANGUAGES_ACT_MAX           (7)  // Actual number of languages: Japanese, English, French, Italian, German, Spanish and Korean
+#define VERSIONS_MAX                (3)  // Number of versions: Diamond, Pearl and Platinum
+#define ASLR_GROUPS_MAX             (12) // Number of ASLR groups (mirrors)
+
 /* Profile slots states */
 #define PSS_EMPTY                    (0x00)
 #define PSS_TAKEN                    (0x01)
@@ -75,10 +87,10 @@ enum {
     PROFILE_BAD_ASLR,
     PROFILE_BAD_LANGUAGE,
     PROFILE_BAD_VERSION,
-    PROFILE_BAD_WILD
+    PROFILE_BAD_WILD,
+    PROFILE_BAD_MAC
 };
 
-/* TODO: replace this by a macro? */
 const u8 PROFILE_ErrorCodes[12][STRING_LENGTH_MAX] = {
     "OK",
     "FRAMES",
@@ -94,7 +106,7 @@ const u8 PROFILE_ErrorCodes[12][STRING_LENGTH_MAX] = {
     "WILD"
 };
 
-//TODO: RESULTDATA should be in motor.h but it needs to be used by io.h
+//RESULTDATA should be in motor.h but it needs to be used by io.h
 typedef struct {
     u32 seed;
     u32 pid;
@@ -113,18 +125,19 @@ typedef struct {
 } RESULTDATA;
 
 typedef struct {
-    u32 frames;
-    u32 seed;
-    u16 filter_species;
-    u16 filter_move;
-    u16 filter_item;
-    u16 tid;
-    u16 sid;
-    u8 aslr;
-    u8 language;
-    u8 version;
-    u8 wild;
-    u8 padding[10];
+    u32 frames; //number of frames to search
+    u32 seed; //starting search seed
+    u16 filter_species; //index of species to filter
+    u16 filter_move; //index of move to filter
+    u16 filter_item; //index of item to filter
+    u16 tid; //original trainer id
+    u16 sid; //original trainer secret id
+    u8 aslr; //aslr index
+    u8 language; //game language index
+    u8 version; //game version index
+    u8 wild; //original wild target to be corrupted
+    u8 mac[6]; //mac address
+    u8 padding[4];
     //there's space for an IV filter (4 bytes)
     //Size: 32 bytes, padded to be the same size as RESULTDATA on purpose
 } PROFILE;
@@ -155,9 +168,9 @@ static void SetString(u8* dest, u16 val, const u8 strarr[][STRING_LENGTH_MAX], u
     return;
 }
 
-static BOOL IsValidIntString_hex(u8 b[U32_DIGITS_HEX_MAX]) {
+static BOOL IsValidIntString_hex(u8* b, u32 size) {
     /* Valid characters are 0~9 and A~F */
-    for (u8 i = 0; i < U32_DIGITS_HEX_MAX; i++) {
+    for (u8 i = 0; i < size; i++) {
         if (b[i] < '0' || b[i] > 'F') { return FALSE; }
         if (b[i] > '9' && b[i] < 'A') { return FALSE; }
     }
@@ -201,13 +214,29 @@ static u8 HexLetterToNumber(u8 n) {
     }
 }
 
+static u32 BCD(u32 val) {
+    /* Binary Coded Decimal for values under 100 */
+    //todo: macro?
+    return val + 6 * (val / 10);
+}
+
 static u32 AsciiToInt_hex32(u8 b[U32_DIGITS_HEX_MAX]) {
     /* Converts a buffer of 8 chars in hex format to an int */
     u32 r = HexLetterToNumber(b[0]);
     for (u8 i = 1; i < U32_DIGITS_HEX_MAX; i++) {
         r = r * 16 + HexLetterToNumber(b[i]);
     }
-    return r - 858993456; //kek
+    return r - 0x33333330; //kek
+}
+
+static u64 AsciiToInt_hex64(u8 b[MAC_DIGITS_HEX_MAX]) {
+    /* Converts a buffer of 8 chars in hex format to an int */
+    u64 r = HexLetterToNumber(b[0]);
+    for (u8 i = 1; i < MAC_DIGITS_HEX_MAX; i++) {
+        r = r * 16 + HexLetterToNumber(b[i]);// - 48;
+    }
+    //return r;
+    return r - 0x3333333333330ULL;
 }
 
 static void ZeroLeftPadTextInputInt(u8* b, u8 size) {
